b6360bd33f94e2448aeb04921cfb248141aa3f3d
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-30 20:54:30 +0300

    Scripted auto-commit on change (2020-10-30 20:54:30) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/JoinJob.java
+++ src/main/java/ru/bmstu/hadoop/JoinJob.java
@@ -4,14 +4,12 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.io.Serializable;
-
-public class JoinJob implements Serializable {
+public class JoinJob {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("Airport flight statistics");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> totalInformationFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaRDD<String> totalInformationFile = sc.textFile("664600583_T_ONTIME_sample.csv");
+        JavaRDD<String> airportNames = sc.textFile("L_AIRPORT_ID.csv");
         JavaRDD<String, String> airports =
     }
 }
