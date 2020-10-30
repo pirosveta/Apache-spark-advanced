5d781ba545b52ef4cfc720b452f606f7cfe92070
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-30 20:27:27 +0300

    Scripted auto-commit on change (2020-10-30 20:27:27) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/JoinJob.java
+++ src/main/java/ru/bmstu/hadoop/JoinJob.java
@@ -1,10 +1,11 @@
 package ru.bmstu.hadoop;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class JoinJob {
     SparkConf conf = new SparkConf().setAppName("Airport flight statistics");
     JavaSparkContext sc = new JavaSparkContext(conf);
-    sc.
+    JavaRDD<String> 
 }
