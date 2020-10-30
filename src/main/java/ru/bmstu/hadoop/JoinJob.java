16ee3440bd9c34189bb95a63e62ad1f94675dd01
pirosveta <svetiklanka.2000@yandex.ru>
2020-10-30 21:02:01 +0300

    Scripted auto-commit on change (2020-10-30 21:02:01) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/JoinJob.java
+++ src/main/java/ru/bmstu/hadoop/JoinJob.java
@@ -13,6 +13,6 @@ public class JoinJob {
         JavaRDD<String> airportNames = sc.textFile("L_AIRPORT_ID.csv");
 
         int column = 0;
-        JavaRDD<String> airports = totalFile.mapToPair(s -> new Tuple2<>(s, column++));
+        JavaRDD<String> airports = totalFile.mapToPair(s -> new Tuple2<>(column++, s));
     }
 }
