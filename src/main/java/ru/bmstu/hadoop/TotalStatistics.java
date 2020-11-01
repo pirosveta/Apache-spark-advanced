c11b4d591717be87a1543f798cfdc9e4c5fe8fc5
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-01 20:09:52 +0300

    Scripted auto-commit on change (2020-11-01 20:09:52) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/TotalStatistics.java
+++ src/main/java/ru/bmstu/hadoop/TotalStatistics.java
@@ -1,10 +1,11 @@
 package ru.bmstu.hadoop;
 
 public class TotalStatistics {
-    private int maxDelay, totalDelayedCancelledFlights = 0, totalFlights, percentDelayedCancelledFlights;
+    private int maxDelay, totalDelayedCancelledFlights = 0, totalFlights = 0, percentDelayedCancelledFlights;
 
     public TotalStatistics(int delay, int cancelled) {
         maxDelay = delay;
+        totalFlights++;
         if (delay > 0 || cancelled == 1) {
             totalDelayedCancelledFlights++;
         }
