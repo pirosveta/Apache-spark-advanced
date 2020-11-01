0205d6fb0331e624f4252ec69c9f80528b081a87
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-01 20:59:28 +0300

    Scripted auto-commit on change (2020-11-01 20:59:28) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/JoinJob.java
+++ src/main/java/ru/bmstu/hadoop/JoinJob.java
@@ -19,10 +19,10 @@ public class JoinJob {
         JavaPairRDD<Tuple2<String, String>, SingleStatistics> orderedTotalData = parsedTotalData.mapToPair(s ->
                 new Tuple2<>(new Tuple2<>(s.getOriginAirportID(), s.getDestAirportID()),
                 new SingleStatistics(s.getDelay(), s.getCancelled())));
-        Map<Tuple2<String, String>, TotalStatistics> airportDataMap = orderedTotalData.combineByKey(
+        Map<Tuple2<String, String>, TotalStatistics> airportData = orderedTotalData.combineByKey(
                 TotalStatistics::new,
                 TotalStatistics::updateStatistics,
                 TotalStatistics::update).collectAsMap();
-        final Broadcast<Map<String, AirportData>> airportsBroadcasted = sc.broadcast(airportDataMap);
+        final Broadcast<Map<String, AirportData>> airportsBroadcasted = sc.broadcast(airportNames);
     }
 }
