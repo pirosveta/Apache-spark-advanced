6077108839dc69c90c7ee326eb6f4330b61982b2
pirosveta <svetiklanka.2000@yandex.ru>
2020-11-02 23:10:10 +0300

    Scripted auto-commit on change (2020-11-02 23:10:10) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/ParsedNames.java
+++ src/main/java/ru/bmstu/hadoop/ParsedNames.java
@@ -3,10 +3,13 @@ package ru.bmstu.hadoop;
 import scala.Serializable;
 
 public class ParsedNames implements Serializable {
+    private static final String REGEX_FOR_REPLACE = "\"", REPLACE = "";
+    private static final int 
+
     private String airportID, airportName;
 
     public ParsedNames(String[] data) {
-        airportID = data[0].replaceAll("\"", "");
+        airportID = data[0].replaceAll(REGEX_FOR_REPLACE, REPLACE);
         airportName = data[1];
     }
 
